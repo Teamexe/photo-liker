580a49e67ee1497001945487e83d76f87fbfd66c
Rashi Sah <rashi.747@gmail.com>
2017-03-30 15:21:43 +0530

    Updated

--- db_conn.php
+++ db_conn.php
@@ -6,6 +6,9 @@ $dbpass = "mpsk22";
 $dbname = "photo_liker";
 
 $con = mysqli_connect("$dbhost","$dbuser","$dbpass","$dbname") or die("Some error occurred during connection " . mysqli_error($con));  
-
+if(mysqli_connect_errno($connect))
+{
+		echo 'Failed to connect';
+}
 
 ?>
